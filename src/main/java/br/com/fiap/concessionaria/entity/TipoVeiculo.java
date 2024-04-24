1b8d6cd2a979a346d23d0b03dacb204e4b240c73
DiegoIacabo <diegoiacabo@gmail.com>
2024-04-24 11:48:22 -0300

    Mapeamento para o Banco de Dados

--- src/main/java/br/com/fiap/concessionaria/entity/TipoVeiculo.java
+++ src/main/java/br/com/fiap/concessionaria/entity/TipoVeiculo.java
@@ -1,5 +1,6 @@
 package br.com.fiap.concessionaria.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +11,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity
+@Table(name = "TB_TIPO_VEICULO", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "UK_NM_TIPO", columnNames = "NM_TIPO"
+        )
+})
 public class TipoVeiculo {
 
+    @Id
+    @Column(name = "ID_TIPO")
+    @GeneratedValue(generator = "SQ_TIPO", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "SQ_TIPO", sequenceName = "SQ_TIPO", allocationSize = 1)
     private Long id;
 
+    @Column(name = "NM_TIPO")
     private String nome;
 }
